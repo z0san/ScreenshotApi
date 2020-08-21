716fc3611985f862c15f3556e1bd609581e9dbd0
Zohar Singer <injapanthree@gmail.com>
2020-08-21 14:45:01 -0400

    fix config

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 var config = {};
 
-// config.appId =  "__APP_ID__";
-// config.appSecret = "__APP_SECRET__";
-//
-// config.keyFile = "__PATH_TO_KEY_FILE__";
-// config.fullchainFile = "__PARTH_TO_FULL_CHAIN__"
-
 config.dbUser = "__DATABASE_USERNAME__";
 config.dbPass = "__DATABASE_PASSWORD__";
 
