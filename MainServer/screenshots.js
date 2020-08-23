0c3d602ba5be22f2470d2d44237fef0c2698d64e
Zohar Singer <injapanthree@gmail.com>
2020-08-23 14:37:12 -0400

    finally got the suffix working with many callbacks
    also get callbacks now

--- MainServer/screenshots.js
+++ MainServer/screenshots.js
@@ -33,26 +33,17 @@ router.get('/', function(req, res){
    function(err, response){
      //test to see if there are any results for required url
      if(response.length == 0){
+        //stores the hash of the url for faster file managment
+        var hash = 'fake hash';
+
        //adds url to db
        if(!url){ //needs to make sure it's a valid url
            res.send('please enter a valid url');
         } else {
-           var newUrl = new UrlModel({
-              url: url,
-              hash: "fake hash",
-              suffix: 0,
-              imgLink: "screenshots/testImage.jpg",
-              downloaded: true,
-              assigned: false,
-              downloader: null
-           });
-
-           newUrl.save(function(err, Person){
-              if(err)
-                 res.send("Database error:  " + type);
-              else
-                 res.send("New person url added!");
-           });
+          //call save new url funciton to create a new entry for this url
+          saveNewUrl(UrlModel, url, hash, function(err, response){
+            res.send("New person url added!");
+          });
         }
      }else{ //if there is already an entry in db for specified url send url
        console.log(response[0]);
@@ -65,3 +56,36 @@ router.get('/', function(req, res){
 
 //Routes will go here
 module.exports = router;
+
+
+//function to save new url entry
+function saveNewUrl(UrlModel, url, hash, callback){
+  getSuffix(UrlModel, hash, function(err, response, len){
+    var newUrl = new UrlModel({
+       url: url,
+       hash: hash,
+       suffix: len,
+       imgLink: "screenshots/testImage.jpg",
+       downloaded: true,
+       assigned: false,
+       downloader: null
+    });
+
+    newUrl.save(function(err, Person){
+       if(err)
+          return ("Database error:  " + type);
+       else
+        callback(err, response);
+    });
+  })
+}
+
+//function to check for hash collisions and come up with suffix to fix this problem
+function getSuffix(UrlModel, hash, callback){
+  var len = 0;
+  UrlModel.find({hash: hash}, function(err, response){
+    len = response.length; //this will always return the next int for a suffix with common hash
+    console.log('response length: ' + len);
+    callback(err, response, len);
+  });
+}
