5f65c17949974729bbc1c128ee0a7b9443098b23
Zohar Singer <injapanthree@gmail.com>
2020-09-10 12:16:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,8 @@ config.dbPass = "__DATABASE_PASSWORD__";
 
 config.dbName = "__DATABASE_NAME__";
 
+
+config.keyFile = "__PATH_TO_KEYFILE__";
+config.fullchainFile = "__PATH_TO_FULLCHAIN_FILE";
+
 module.exports = config;
